0a1afb8af8b1ebd8667a2f1ffcbd6b69a7cf8eb5
Thug-Okami <m.senna.cordeiro@gmail.com>
2024-09-19 11:54:55 -0300

    começo da branch

---
+++ src/pages/teste.jsx
@@ -0,0 +1,16 @@
+import './SwapySandbox.css';
+
+import SwapyContainer from '../components/swapy/SwapyContainer';
+import SwapySlot from '../components/swapy/SwapySlot';
+import SwapyItem from '../components/swapy/SwapyItem';
+
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+
+const OkamiTeste = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+};
