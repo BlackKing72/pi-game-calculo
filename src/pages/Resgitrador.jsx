f22512d5693674a9bfcfd82a8d9e37b2fc3d3790
Thug-Okami <m.senna.cordeiro@gmail.com>
2024-09-24 21:25:18 -0300

    mudança de CSS

--- src/pages/Resgitrador.jsx
+++ src/pages/Resgitrador.jsx
@@ -7,14 +7,14 @@ const Registrador = () => {
     return(
         <div>
             <div className="TabelaResponsivo">
-                <table className="flex flex-col gap-10">
+                <table className="table-auto border-collapse border-spacing-2 border border-slate-500 border-spacing-2">
                     <thead>
-                        <tr className="gap-10">
-                            <th>Id</th>
-                            <th>Enunciado</th>
-                            <th>tipo de pergunta</th>
-                            <th>Data de alteração</th>
-                            <th>Ações</th>
+                        <tr>
+                            <th className="border border-slate-600">Id</th>
+                            <th className="border border-slate-600">Enunciado</th>
+                            <th className="border border-slate-600">tipo de pergunta</th>
+                            <th className="border border-slate-600">Data de alteração</th>
+                            <th className="border border-slate-600">Ações</th>
                         </tr>
                     </thead>
                     <tbody>
