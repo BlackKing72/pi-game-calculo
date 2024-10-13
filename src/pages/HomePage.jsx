e9ad67d304143c3acbc3ffbc26b630bcd0c9d632
BlackKing72 <matheus.csilv14@gmail.com>
2024-10-13 11:58:42 -0300

    Adicionando o logo na página inicial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { LoadingScreen } from '@/components/ui/loading';
 import { GameButton } from '@/components/game/game-button';
 
+import owlpostLogo from '/owlpost-white.png';
+
 import * as perguntasService from "../services/perguntasService";
 
 const HomePage = () => {
@@ -24,8 +26,8 @@ const HomePage = () => {
 
     const opcoes = [
         { nome: 'Jogar',    onClick: handleOnClickJogar,  enabled: true},
-        { nome: 'Tutorial', onClick: () => { }, enabled: true},
-        { nome: 'Opções',   onClick: () => { }, enabled: true},
+        { nome: 'Tutorial', onClick: () => { }, enabled: false},
+        { nome: 'Opções',   onClick: () => { }, enabled: false},
     ]
 
     return loading
@@ -36,6 +38,9 @@ const HomePage = () => {
 
                 <div className="w-full flex flex-col flex-grow gap-4 max-w-[50ch] justify-center p-4">
                     <div className="bg-[#f8fafc10] text-slate-50 backdrop-blur-[4px] z-10 rounded-lg p-10 flex flex-col gap-4 items-center ring-1 ring-[#f8fafc40]">
+                        <div className="w-40 aspect-square">
+                            <img src={owlpostLogo}/>
+                        </div>
                         <h1 className="font-bold">OwlCalc</h1>
                         <hr className="mb-8 w-full"/>
                         {
